21fe4f524950bd19610e7289bc0320543bf20fa2
Joseph Amegatcher <jgatcher@gmail.com>
2012-06-19 15:09:56 +0000

    cleaned up the config set to mongloabs dev db

--- application/config/mongo_db.php
+++ application/config/mongo_db.php
@@ -9,19 +9,19 @@
 // $config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-// $config['mongo_host'] = 'ds033877.mongolab.com:33877/teencamp_dev';
-// $config['mongo_port'] = '';
-// $config['mongo_db'] = 'teencamp_dev';
-// $config['mongo_user'] = 'arcyf';
-// $config['mongo_pass'] = 'connecting';
-// $config['mongo_persist'] = TRUE;
-// $config['mongo_persist_key'] = 'ci_mongo_persist';
+ $config['mongo_host'] = 'ds033877.mongolab.com:33877/teencamp_dev';
+ $config['mongo_port'] = '';
+ $config['mongo_db'] = 'teencamp_dev';
+ $config['mongo_user'] = 'arcyf';
+ $config['mongo_pass'] = 'connecting';
+ $config['mongo_persist'] = TRUE;
+ $config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-$config['mongo_host'] = 'mongo ds033887.mongolab.com:33887/teencamp_live';
-$config['mongo_port'] = '';
-$config['mongo_db'] = 'teencamp_live';
-$config['mongo_user'] = 'arcyf';
-$config['mongo_pass'] = 'connecting';
-$config['mongo_persist'] = TRUE;
-$config['mongo_persist_key'] = 'ci_mongo_persist';
+//$config['mongo_host'] = 'ds033887.mongolab.com:33887/teencamp_live';
+//$config['mongo_port'] = '';
+//$config['mongo_db'] = 'teencamp_live';
+//$config['mongo_user'] = 'arcyf';
+//$config['mongo_pass'] = 'connecting';
+//$config['mongo_persist'] = TRUE;
+//$config['mongo_persist_key'] = 'ci_mongo_persist';
