e4fc202465d62b4cbec536251c114e66c0ea2cd6
Joseph Amegatcher <jgatcher@gmail.com>
2012-06-19 12:48:56 +0000

    modified db config to teencamp

--- application/config/mongo_db.php
+++ application/config/mongo_db.php
@@ -2,25 +2,25 @@
 
 // $config['mongo_host'] = 'localhost';
 // $config['mongo_port'] = 27017;
-// $config['mongo_db'] = 'youthcamp_live';
+// $config['mongo_db'] = 'teencamp_dev';
 // $config['mongo_user'] = '';
 // $config['mongo_pass'] = '';
 // $config['mongo_persist'] = TRUE;
 // $config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-// $config['mongo_host'] = 'ds031857.mongolab.com:31857/youthcamp_dev';
+// $config['mongo_host'] = 'ds033877.mongolab.com:33877/teencamp_dev';
 // $config['mongo_port'] = '';
-// $config['mongo_db'] = 'youthcamp_dev';
-// $config['mongo_user'] = 'soundfever';
-// $config['mongo_pass'] = 'soundfever18';
+// $config['mongo_db'] = 'teencamp_dev';
+// $config['mongo_user'] = 'arcyf';
+// $config['mongo_pass'] = 'connecting';
 // $config['mongo_persist'] = TRUE;
 // $config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-$config['mongo_host'] = 'ds033217.mongolab.com:33217/youthcamp_live';
+$config['mongo_host'] = 'mongo ds033887.mongolab.com:33887/teencamp_live';
 $config['mongo_port'] = '';
-$config['mongo_db'] = 'youthcamp_live';
+$config['mongo_db'] = 'teencamp_live';
 $config['mongo_user'] = 'arcyf';
 $config['mongo_pass'] = 'connecting';
 $config['mongo_persist'] = TRUE;
