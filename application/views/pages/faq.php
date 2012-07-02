40e868ad939eb5e767ea57fbf752514952f1bbe2
Joseph Amegatcher <jgatcher@gmail.com>
2012-07-02 12:12:59 +0000

    edited faq

--- application/views/pages/faq.php
+++ application/views/pages/faq.php
@@ -10,8 +10,7 @@
 				</li>
 				<li>
 					<div class='question'>When is the deadline for registration?</div>
-					<div class='answer'>Deadline for registration is 10th June 2012. 
-						Deadline for registration is Friday, 10th August 2012. 
+					<div class='answer'>Deadline for registration is Friday, 10th August 2012. 
 						Registration is only complete upon payment and failure to make payment before the
 						registration deadline will result in a forfeiture of registrant’s place at camp.
 						To avoid this situation, campers are required to inform registration officers beforehand of their inability to meet
@@ -36,21 +35,15 @@
 					<div class='answer'>There is currently no provision for sponsorship. However campers who</div>
 				</li> -->
 				<li>
-					<div class='question'>Will I still have to pay GHC60 if I don’t make it on Wednesday?</div>
-					<div class='answer'> Campers who will be at camp anytime between Thursday (14th June ) and Saturday (16th June) 
-						will be required to pay a camp fee of GHC50.
-					</div>
-				</li>
-				<li>
 					<div class='question'>Can I receive a refund after having made payment?</div>
 					<div class='answer'>Camp fees once paid are not refundable.</div>
 				</li>
 					<li>
 					<div class='question'>Can I attend camp even if I haven’t made payment?</div>
 					<div class='answer'> Campers who for one reason or other cannot make payments before the deadline are required to inform the following 
-					camp officers, <b>Kukua Pratt 0244574675, Anthony Davor 0244226692, Andrew Amegatcher 0267666975 <b> latest by the close of 
-					day on the Friday, 10th of August, 2012. 
-					<br/>Campers who for one reason or other cannot make payments before the deadline are required to inform the following camp officers, Kukua Pratt 0244574675, Anthony Davor 0244226692, Andrew Amegatcher 0267666975 latest by the close of day on the Friday, 10th of August, 2012. 
+					camp officers, <b>Kukua Pratt 0244574675, Anthony Davor 0244226692, Andrew Amegatcher 0267666975 </b> 
+					latest by the close of 	day on the Friday, 10th of August, 2012. 
+
 					</div>
 				</li>
 				<!-- <li>
