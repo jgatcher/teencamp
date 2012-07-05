54748ccc2b93408eb7685d48fe021b116fa6c822
Joseph Amegatcher <jgatcher@gmail.com>
2012-07-05 16:07:20 +0000

    checkedout ea1d4e version of faq

--- application/views/pages/faq.php
+++ application/views/pages/faq.php
@@ -11,10 +11,10 @@
 				<li>
 					<div class='question'>When is the deadline for registration?</div>
 					<div class='answer'>Deadline for registration is Friday, 10th August 2012. 
-								Registration is only complete upon payment and failure to make payment before the 
-								registration deadline will result in a forfeiture of registrant’s place at camp. 
-								To avoid this situation, campers are required to inform registration officers 
-								beforehand of their inability to meet the deadline. 
+						Registration is only complete upon payment and failure to make payment before the
+						registration deadline will result in a forfeiture of registrant’s place at camp.
+						To avoid this situation, campers are required to inform registration officers beforehand of their inability to meet
+						the deadline. 
 					</div>
 				</li>
 				<li>
@@ -23,17 +23,21 @@
 						to Friday and on Sundays at registration desk.
 					</div>
 				</li>
-				
-
+				<li>
+					<div class='question'>Are there any discounts for families with multiple members attending camp?</div>
+					<div class='answer'>  
+						Yes, families with 3 or more members attending camp will receive a 15% discount 
+						on the total amount they would have had to pay.
+					</div>
+				</li>
+				<!-- <li>
+					<div class='question'>What do I do if I want to attend camp but can’t afford it?</div>
+					<div class='answer'>There is currently no provision for sponsorship. However campers who</div>
+				</li> -->
 				<li>
 					<div class='question'>Can I receive a refund after having made payment?</div>
 					<div class='answer'>Camp fees once paid are not refundable.</div>
 				</li>
-				<li>
-					<div class='question'>Can I attend camp even if I haven’t made payment?</div>
-					<div class='answer'> Campers who for one reason or other cannot make payments before the deadline are required to 
-						inform the following camp officers, Kukua Pratt 0244574675, Anthony Davor 0244226692, Andrew Amegatcher 0267666975 
-						latest by the close of day on the Friday, 10th of August, 2012. 
 					<li>
 					<div class='question'>Can I attend camp even if I haven’t made payment?</div>
 					<div class='answer'> Campers who for one reason or other cannot make payments before the deadline are required to inform the following 
@@ -43,7 +47,10 @@
 
 					</div>
 				</li>
-				
+				<!-- <li>
+					<div class='question'></div>
+					<div class='answer'></div>
+				</li> -->
 			</ol>	
 		</div>		
 	</div>
