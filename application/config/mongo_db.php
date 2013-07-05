e8edd3ee62d8e11acc7b7d33d7440d871fffcd65
jgatcher <jgatcher@gmail.com>
2013-07-05 17:10:41 +0000

    Update mongo_db.php

--- application/config/mongo_db.php
+++ application/config/mongo_db.php
@@ -18,9 +18,9 @@
  // $config['mongo_persist_key'] = 'ci_mongo_persist';
 
 
-$config['mongo_host'] = 'ds033887.mongolab.com:33887/teencamp_live';
+$config['mongo_host'] = 'ds033018.mongolab.com:33018/teencamp13_live';
 $config['mongo_port'] = '';
-$config['mongo_db'] = 'teencamp_live';
+$config['mongo_db'] = 'teencamp13_live';
 $config['mongo_user'] = 'arcyf';
 $config['mongo_pass'] = 'connecting';
 $config['mongo_persist'] = TRUE;
