a3090e32dd07a0f7439c8114f1846b4c74e8c08b
jgatcher <jgatcher@gmail.com>
2013-07-25 05:59:58 +0000

    Update faq.php

--- application/views/pages/faq.php
+++ application/views/pages/faq.php
@@ -6,11 +6,11 @@
 			<ol>
 				<li>
 					<div class='question'>How much will it cost to attend camp this year?</div>
-					<div class='answer'>This year’s camp will cost each camper a highly subsidised fee of GHC60.</div>
+					<div class='answer'>This year’s camp will cost each camper a highly subsidised fee of GHC75.</div>
 				</li>
 				<li>
 					<div class='question'>When is the deadline for registration?</div>
-					<div class='answer'>Deadline for registration is Friday, 10th August 2012. 
+					<div class='answer'>Deadline for registration is Monday, 2nd September 2013. 
 						Registration is only complete upon payment and failure to make payment before the
 						registration deadline will result in a forfeiture of registrant’s place at camp.
 						To avoid this situation, campers are required to inform registration officers beforehand of their inability to meet
@@ -41,8 +41,8 @@
 					<li>
 					<div class='question'>Can I attend camp even if I haven’t made payment?</div>
 					<div class='answer'> Campers who for one reason or other cannot make payments before the deadline are required to inform the following 
-					camp officers, <b>Kukua Pratt 0244574675, Anthony Davor 0244226692, Andrew Amegatcher 0267666975 </b> 
-					latest by the close of 	day on the Friday, 10th of August, 2012. 
+					camp officers, <b>Edem Aperwokin 0242112111, Baffour 0272262233, Aaron Anim 0262920697 </b> 
+					latest by the close of 	day on the Monday, 2nd of September, 2013. 
 					<br/>Registrants who fail to make payment arrangements with the listed camp officers will be considered to have redrawn their application. 
 
 					</div>
